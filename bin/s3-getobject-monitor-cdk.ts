c3e1a19303169416abbd680bdaa2a8ad69adb5fa
shinkawa <shinkawa.misaki@gmail.com>
2025-09-06 22:10:36 +0900

    Update s3-getobject-monitor-cdk.ts

--- bin/s3-getobject-monitor-cdk.ts
+++ bin/s3-getobject-monitor-cdk.ts
@@ -1,6 +1,6 @@
 /*
  * SPDX-License-Identifier: MIT
- * Copyright (c) 2025 <Your Name or Org>
+ * Copyright (c) 2025 <shinkawa>
  */
 import * as cdk from 'aws-cdk-lib';
 import { S3GetObjectMonitorStack } from '../lib/s3-getobject-monitor-cdk-stack';
